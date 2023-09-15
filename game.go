8285abf9cdc69620e77bc3f417bcc99bdf639424
Alex Jensen <alex@endure.io>
2023-09-14 21:24:34 -0400

    moves can be made on board

--- game.go
+++ game.go
@@ -1,3 +1,16 @@
 package ttgo
 
-type board [9]int
+type piece byte
+
+const (
+	blank = piece(0)
+	x     = piece(1)
+	o     = piece(2)
+)
+
+type board [9]piece
+
+func (b board) move(i int, p piece) board {
+	b[i] = p
+	return b
+}
--- game_test.go
+++ game_test.go
@@ -14,10 +14,22 @@ func (b board) isEmpty() bool {
 
 func TestInitializesBoardOfSizeNine(t *testing.T) {
 	var b board
-	equals(t, 9, len(b))
+	assertEquals(t, 9, len(b))
 }
 
 func TestInitializesBoardBlank(t *testing.T) {
 	var b board
 	assert(t, b.isEmpty())
+}
+
+func TestTokensAssigned(t *testing.T) {
+	assertEquals(t, piece(0), blank)
+	assertEquals(t, piece(1), x)
+	assertEquals(t, piece(2), o)
+}
+
+func TestMovePlayedOnBlankBoard(t *testing.T) {
+	var blankBoard board
+	var playedBoard = blankBoard.move(0, x)
+	assertEquals(t, x, playedBoard[0])
 }
